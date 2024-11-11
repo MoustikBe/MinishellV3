6be2d58d6e8b6498ed68dde68a67a794b5c6f242
Maxence Isaac Castiau <misaac-c@c1r12s3.42madrid.com>
2024-11-11 18:38:18 +0100

    updating multi pipe

--- src/executer/exec_pipe_multi.c
+++ src/executer/exec_pipe_multi.c
@@ -1,48 +1,36 @@
 #include "../../minishell.h"
 
 
-
-
-void	step_1(pid_t pid, t_token *token, t_shell *shell, char *file_in)
+void	step_1(pid_t pid, t_token *token, t_shell *shell)
 {
-	int i;
-	int fd_in;
+	int	fd_in;
 	char *path;
 	char *cmd_join;
 	char **cmd_exec;
 
 	if (pid == 0)
 	{
-		if (file_in) 
-		{
-    	    fd_in = open(file_in, O_RDONLY);
-    	    if (fd_in < 0) 
-			{
-    	        perror("open");
-    	        exit(EXIT_FAILURE);
-    	    }
-    	    dup2(fd_in, STDIN_FILENO);
-    	    close(fd_in);
-    	}
-    	// Duplication de la sortie du pipe vers stdout
-    	dup2(shell->fd[1], STDOUT_FILENO);
-    	close(shell->fd[0]);
+		dup2(shell->fd[1], STDOUT_FILENO);
+   		close(shell->fd[0]);
     	close(shell->fd[1]);
 		// EXECUTION DE LA COMMANDE
-		i = 0;
+		shell->index = 0;
 		cmd_join = calloc(1, 1);
-		while(token[i].id != 6)
+		while(token[shell->index].id != 6)
 		{	
-			cmd_join = ft_strjoin(cmd_join, token[i].str);
+			cmd_join = ft_strjoin(cmd_join, token[shell->index].str);
 			cmd_join = ft_strjoin(cmd_join, " ");
-			i++;
+			shell->index++;
 		}
+
+		///fprintf(stderr, "-> %s\n", path);
 		cmd_exec = ft_split(cmd_join, ' ');
+		//fprintf(stderr, "cmd_exec -> %s, path -> %s\n", cmd_exec[0], path);
 		if(check_cmd_quotes(cmd_exec[0]) > 1)
 		{
 			//printf("BUILTIN DETECTED\n");
 			if(check_cmd_quotes(cmd_exec[0]) == 2)
-				echo(token, i);
+				echo(token, shell->index);
 			else if(check_cmd_quotes(cmd_exec[0]) == 3)
 				cd(token[1].str);
 			else if(check_cmd_quotes(cmd_exec[0]) == 4)
@@ -52,6 +40,13 @@ void	step_1(pid_t pid, t_token *token, t_shell *shell, char *file_in)
 		path = make_path(cmd_exec[0]);
     	execve(path, cmd_exec, NULL);
 	}
+}
+
+void	to_nb_cmd(t_shell *shell)
+{
+	close(shell->fd[1]);
+	shell->fd_temp = shell->fd[0];
+	pipe(shell->fd);
 }
 
 void	command_execution(t_token *token, t_shell *shell)
@@ -87,39 +82,47 @@ void	command_execution(t_token *token, t_shell *shell)
 }
 
 
-void createprocessus(t_shell *shell, t_token *token, pid_t pid)
+void	createprocessus(pid_t pid, t_shell *shell, t_token *token)
 {
-    if (pid == 0)
-    {
-        close(shell->fd[0]);
-        dup2(shell->fd_temp, STDIN_FILENO);
-        dup2(shell->fd[1], STDOUT_FILENO);
-        close(shell->fd_temp); // Fermez l'entrée temporaire après dup2
-        close(shell->fd[1]);    // Fermez l'extrémité écriture après dup2
-
-        command_execution(token, shell);
-        exit(0);
-    }
-    close(shell->fd[1]); // Fermez l'extrémité écriture dans le processus parent
-    shell->fd_temp = shell->fd[0];
-    if (shell->nb_cmd > 1) {
-        pipe(shell->fd);
-    }
-    shell->nb_cmd--;
+	if (pid == 0)
+	{
+		close(shell->fd[0]);
+		dup2(shell->fd_temp, STDIN_FILENO);
+		dup2(shell->fd[1], STDOUT_FILENO);
+		close(shell->fd_temp);
+		close(shell->fd[1]);
+		command_execution(token, shell);
+	}
+	if (shell->nb_cmd > 1)
+	{
+		close(shell->fd_temp);
+		close(shell->fd[1]);
+		shell->fd_temp = shell->fd[0];
+		pipe(shell->fd);
+	}
+	shell->nb_cmd--;
+	//fprintf(stderr, "shell.index %d | token.str -> %s\n", shell->index, token[shell->index].str);
+	while (token[shell->index].id != 6)
+		shell->index++;
+	shell->index++;
 }
 
 void	sub_process(t_shell *shell, t_token *token, pid_t pid)
 {
-	fprintf(stderr ,"shell->nb_cmd -> %d\n", shell->nb_cmd);
+	if(shell->index == 0)
+	{
+		while(token[shell->index].id != 6)
+			shell->index++;
+	}
+	shell->index++;
 	while (shell->nb_cmd)
 	{
 		pid = fork();
 		if (pid == -1)
 			exit(2);
-		fprintf(stderr, "shell->index -> %d\n token -> %s\n", shell->index, token[shell->index].str);
 		if(token[shell->index].id == 6)
 			shell->index++;
-		createprocessus(shell, token, pid);
+		createprocessus(pid, shell, token);
 	}
 }
 
@@ -149,65 +152,42 @@ void wait_execution(int nb_cmd)
 	int status;
 
 	while(nb_cmd--)
+	{
+		//fprintf(stderr , "waiting\n");		
 		waitpid(-1, &status, 0);
-}
-
-
-void	to_nb_cmd(t_shell *shell)
-{
-	close(shell->fd[1]);
-	shell->fd_temp = shell->fd[0];
-	pipe(shell->fd);
+	}
 }
 
 void pipex_multi(t_token *token, t_shell *shell)
 {
-	int len;
-	int i;
-	char *file_in;
-	char *file_out;
-	char **split_cmd;
-	pid_t pid;
+    int i;
+    char **split_cmd;
+    pid_t pid;
 
-	file_in = NULL;
-	file_out = NULL;
-	i = 0;
-	while(token[i].str)
-	{
-		if(token[i].id == 5)
-			file_in = ft_strdup(token[i].str);
-		else if(token[i].id == 4)
-			file_out = ft_strdup(token[i].str);
-		i++;
-	}
-	i = 0;
-	split_cmd = ft_split(shell->cmd, '|');
-	while (split_cmd[i])
-		i++;
-	shell->nb_cmd = i - 2;
+	shell->index = 0;
+    i = 0;
+    split_cmd = ft_split(shell->cmd, '|');
+    while (split_cmd[i])
+        i++;
+    shell->nb_cmd = i - 2;
+    pipe(shell->fd);
+    pid = fork();
+    if (pid == -1)
+        printf("ERROR\n");
 
-	pipe(shell->fd);
-	pid = fork();
-	if(pid == -1)
-		printf("ERROR\n"); 
-
-	/* Work in progress */
-	
-	// SAVOIR GERER PLUSIEURS INFILE ET OUTFILE ? COMMENT ? 
-	// Pour commencer faison en sorte de gerer des infiles et outfile. 
-	// Seulement si il se trouve dans la premiere ou derniere etape. 
-	// COPIE de la facon de faire dans le simple pipe. 
-	step_1(pid, token, shell, file_in);
-	to_nb_cmd(shell);
-	//fprintf(stderr, "After_pipe_mngt\n");
-	sub_process(shell, token, pid);
+    step_1(pid, token, shell);
+	//fprintf(stderr, "step1 -> shell.index %d | token.str -> %s\n", shell->index, token[shell->index].str);
+	close(shell->fd[1]);  // Fermez l'extrémité écriture après step_1 dans le parent
+    to_nb_cmd(shell);
+    sub_process(shell, token, pid);
+	//fprintf(stderr, "sub_process -> shell.index %d | token.str -> %s\n", shell->index, token[shell->index].str);
     from_nb_cmd(shell);
-	//fprintf(stderr, "Before last step\n");
+
     pid = fork();
     if (pid == -1)
         exit(2);
 
     last_step(shell, token, pid);
-    wait_execution(shell->nb_cmd + 2);
-    close(shell->fd_temp); 
+    wait_execution(shell->nb_cmd + 3);
+    close(shell->fd_temp); // Fermez l'extrémité temporaire
 }
