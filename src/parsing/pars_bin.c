0301b1b2ab14d73c0bff78a63c1588ab31d42f78
Maxence Isaac Castiau <misaac-c@c1r8s6.42madrid.com>
2024-11-21 11:02:16 +0100

    optimisation

--- src/parsing/pars_bin.c
+++ src/parsing/pars_bin.c
@@ -2,16 +2,11 @@
 
 int direct_bin(char *cmd)
 {
-    int len;
-    int flag_acces;
+    int i;
 
-	//printf("|%s|\n", cmd);
-    len = ft_strlen(cmd);
-    if(len > 4)
-    {
-		if(access(cmd, O_RDONLY) == 0)
-			return(1);
-        return(0);
-    }
-    return(0);
+	i = 0;
+	// Check if PATH EXIST FIRST 
+	if(access(cmd, O_RDONLY) == 0)
+		return(1);
+	return(0);
 }
