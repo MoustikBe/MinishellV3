a4a76ef5ce280ba110e7310085c7242a9d9a5c30
Maxence Isaac Castiau <misaac-c@c2r8s6.42madrid.com>
2024-12-10 12:38:52 +0100

    Updating the binary way for the builtin according to bash

--- src/parsing/pars_cmd.c
+++ src/parsing/pars_cmd.c
@@ -5,17 +5,17 @@ int check_cmd_quotes(char *cmd_check)
 {
 	if(str_cmp_quotes(cmd_check, "echo") == 1 || str_cmp_quotes(cmd_check, "/bin/echo") == 1)
 		return(2);
-	else if(str_cmp_quotes(cmd_check, "cd") == 1 || str_cmp_quotes(cmd_check, "/bin/cd") == 1)
+	else if(str_cmp_quotes(cmd_check, "cd") == 1)
 		return(3);
 	else if(str_cmp_quotes(cmd_check, "pwd") == 1 || str_cmp_quotes(cmd_check, "/bin/pwd") == 1)
 		return(4);
-	else if(str_cmp_quotes(cmd_check, "export") == 1 || str_cmp_quotes(cmd_check, "/bin/export") == 1)
+	else if(str_cmp_quotes(cmd_check, "export") == 1)
 		return(5);
-	else if(str_cmp_quotes(cmd_check, "unset") == 1 || str_cmp_quotes(cmd_check, "/bin/unset") == 1)
+	else if(str_cmp_quotes(cmd_check, "unset") == 1)
 		return(6);
 	else if(str_cmp_quotes(cmd_check, "env") == 1 || str_cmp_quotes(cmd_check, "/bin/env") == 1)
 		return(7);
-	else if(str_cmp_quotes(cmd_check, "exit") == 1 || str_cmp_quotes(cmd_check, "/bin/exit") == 1)
+	else if(str_cmp_quotes(cmd_check, "exit") == 1)
 		return(8);
 	else if(check_syntax(cmd_check) == 1)
 		return(9);
--- src/token/token_id.c
+++ src/token/token_id.c
@@ -165,17 +165,17 @@ int is_cmd(t_token *token, int i)
 
     if(str_cmp(token[i].str, "echo") || str_cmp(token[i].str, "/bin/echo"))
         token[i].id = 11;
-    else if(str_cmp(token[i].str, "cd") || str_cmp(token[i].str, "/bin/cd"))
+    else if(str_cmp(token[i].str, "cd"))
         token[i].id = 12;
     else if(str_cmp(token[i].str, "pwd") || str_cmp(token[i].str, "/bin/pwd"))
         token[i].id = 13;
-    else if(str_cmp(token[i].str, "export") || str_cmp(token[i].str, "/bin/export"))
+    else if(str_cmp(token[i].str, "export"))
         token[i].id = 14;
-    else if(str_cmp(token[i].str, "unset") || str_cmp(token[i].str, "/bin/unset"))
+    else if(str_cmp(token[i].str, "unset"))
         token[i].id = 15;
     else if(str_cmp(token[i].str, "env") || str_cmp(token[i].str, "/bin/env"))
 		token[i].id = 16;
-	else if(str_cmp(token[i].str, "exit") || str_cmp(token[i].str, "/bin/exit"))
+	else if(str_cmp(token[i].str, "exit"))
 		token[i].id = 17;
     else if(check_bin(token[i].str) || direct_bin(token[i].str))
         token[i].id = 10;
